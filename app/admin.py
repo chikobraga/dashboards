97f4cd32066ca8fb4580c9ade06841adc26a21e9
chikobraga <chikob@gmail.com>
2020-03-30 20:58:49 -0300

    update20200330007

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Account, Transactions
+from .models import Account, Transactions, PossessionTitle, TitleAttr
 
 # Register your models here.
 
 admin.site.register(Account)
 admin.site.register(Transactions)
+admin.site.register(PossessionTitle)
+admin.site.register(TitleAttr)
