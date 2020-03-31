7b1d986d76e8f7bcf8cf706e0e4e6c0163578052
chikobraga <chikob@gmail.com>
2020-03-30 21:18:12 -0300

    update20200330012

--- app/models.py
+++ app/models.py
@@ -25,8 +25,8 @@ class Transactions(models.Model):
 class PossessionTitle(models.Model):
     id = models.AutoField(primary_key=True)
     name_title = models.CharField(max_length=30)
-    owner_title = models.BigIntegerField(editable=True, null=True)
-    color = models.CharField(max_length=30, blank=True)
+    owner_title = models.BigIntegerField(null=True, blank=True)
+    color = models.CharField(max_length=30, null=True, blank=True)
 
 
 class TitleAttr(models.Model):
