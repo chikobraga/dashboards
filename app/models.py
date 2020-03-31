fcb459bd075c415b8dd7e5995672f2527de201fd
chikobraga <chikob@gmail.com>
2020-03-30 21:13:15 -0300

    update20200330011

--- app/models.py
+++ app/models.py
@@ -21,9 +21,6 @@ class Transactions(models.Model):
     update_account = models.ForeignKey(Account, related_name='accountnumbers', on_delete=models.CASCADE)
     value = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return self.update_account
-
 
 class PossessionTitle(models.Model):
     id = models.AutoField(primary_key=True)
