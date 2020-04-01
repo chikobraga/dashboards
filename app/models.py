1ede980d847bf6ad9e6cc3a1decdc0f1b8cdf24d
chikobraga <chikob@gmail.com>
2020-04-01 03:20:06 -0300

    update2020033105

--- app/models.py
+++ app/models.py
@@ -50,6 +50,8 @@ class InfoPossession(models.Model):
         ('2', 'Hotel'),
     )
     id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=30, blank=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
     info_possession = models.ForeignKey(PossessionTitle, related_name='id_infopossession', on_delete=models.CASCADE)
     type_info = models.CharField(max_length=1, choices=INFO_TYPE)
 
