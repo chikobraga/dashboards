ca3db93c1c4648929b98aec858d0f3c1b6be046c
chikobraga <chikob@gmail.com>
2020-04-02 01:23:01 -0300

    update2020040103

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,4 @@ class TransactionForm(ModelForm):
     class Meta:
         model = Transactions
         fields = ['transaction','update_account','dest_account','value']
+        transaction = models.CharField(max_length=1, choices='W')
