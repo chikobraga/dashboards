ecc8633994a73e74c3882cd909ff2cef1ce8f8da
chikobraga <chikob@gmail.com>
2020-04-02 01:56:38 -0300

    update2020040103

--- app/forms.py
+++ app/forms.py
@@ -6,12 +6,19 @@ from .models import *
 class TransactionForm(ModelForm):
     class Meta:
         model = Transactions
-        fields = ['transaction','update_account','dest_account','value']
-    transaction = forms.CharField(widget=forms.TextInput(
-        attrs={'value': 'W',
+        fields = ['Transaction','update_account','dest_account','value']
+    transaction = forms.CharField(
+        widget=forms.TextInput(
+        attrs={ 'value': 'W',
                'class': 'form-control',
-               'placeholder': 'Disabled Input',
-               }))
+               'disabled': 'disabled',
+                'type': 'checkbox',
+               })
+        widget=forms.label(
+            attrs={
+                'class': 'control-label col-md-3 col-sm-3 col-xs-12',
+            }
+        ))
     update_account = forms.CharField(max_length=30)
     dest_account = forms.CharField(max_length=30)
     value = forms.CharField(max_length=30)
