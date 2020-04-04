c788d2fd723924d7623b751ef10a7bcfc97a934d
chikobraga <chikob@gmail.com>
2020-04-03 21:13:00 -0300

    update2020040103

--- app/serializers.py
+++ app/serializers.py
@@ -13,20 +13,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'name']
 
 
-class AccountSerializer(serializers.Serializer):
-    acoount = serializers.IntegerField(primary_key=True, editable=True)
-    name = serializers.CharField(max_length=30)
-    balance = serializers.DecimalField(max_digits=10, decimal_places=2)
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.account = validated_data.get('account', instance.account)
-        instance.name = validated_data.get('name', instance.name)
-        instance.balance = validated_data.get('balance', instance.balance)
-        instance.save()
-        return instance
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['account', 'name', 'balance']
