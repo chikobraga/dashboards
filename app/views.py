9976f0ae6126dff6999b26eafa406584c3d2f1ad
chikobraga <chikob@gmail.com>
2020-04-07 00:47:07 -0300

    update2020040405

--- app/views.py
+++ app/views.py
@@ -139,7 +139,6 @@ class TransactionDetail(APIView):
         transaction = self.get_object(pk)
         serializer = TransactionSerializer(transaction, data=request.data)
         if serializer.is_valid():
-            serializer.save()
             return Response(serializer.data)
         return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
 
