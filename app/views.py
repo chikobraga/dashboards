bcae8a82dc25a0f6108cf988b138cd6af4249ed3
chikobraga <chikob@gmail.com>
2020-04-12 05:50:06 -0300

    update2020040801

--- app/views.py
+++ app/views.py
@@ -170,8 +170,10 @@ class PossessionTitleList(APIView):
     def post(self, request, format=None):
         serializer = PossessionTitleSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class TitleAttrList(APIView):
     def get(self, request, format=None):
