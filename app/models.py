7f3966dce9412106084ae77e8e84024be3e62b84
chikobraga <chikob@gmail.com>
2020-04-12 02:36:37 -0300

    update2020040801

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
