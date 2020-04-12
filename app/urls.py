71a6929c9a83184821dd5d98cb187f0bad85374c
chikobraga <chikob@gmail.com>
2020-04-12 05:47:50 -0300

    update2020040801

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('api/transaction/<int:pk>/', views.TransactionDetail.as_view()),
     path('api/attr/', views.TitleAttrList.as_view()),
     path('api/attr/<int:pk>/', views.TitleAttrDetail.as_view()),
-    path('api/infop/', views.PossessionTitleList()),
+    path('api/infop/', views.PossessionTitleList.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
