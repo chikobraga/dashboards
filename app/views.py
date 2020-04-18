9518b2a2cbf673cada311c0ce10e55bc7d8214ca
chikobraga <chikob@gmail.com>
2020-04-18 02:26:33 -0300

    update2020040801

--- app/views.py
+++ app/views.py
@@ -28,7 +28,7 @@ def index(request):
         else:
             _message = 'Invalid login or password, please try again'
     else:
-        _message = 'Invalid login or password, please try again'
+        _message = '1'
 
     context = {'message': _message}
     template = loader.get_template('app/login.html')
