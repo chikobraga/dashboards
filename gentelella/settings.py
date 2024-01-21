30fc04b02f34c8ce16517be013326a77e6e4d69a
root <root@django.admin.com>
2024-01-21 03:17:00 +0000

    Teste

--- gentelella/settings.py
+++ gentelella/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8*md2t)o**67@*yhc(d=f@j95kl(dnf^rmm4s00$-mh_vurb2b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.95.207.226']
+ALLOWED_HOSTS = ['192.168.0.57']
 
 
 # Application definition
